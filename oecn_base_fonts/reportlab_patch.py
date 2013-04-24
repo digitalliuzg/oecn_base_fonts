9c660c489003bff164937c76a9b7ee9d8a5b047d
joshuajan <popkar77@gmail.com>
2013-04-24 14:44:19 +0800

    modify the comment

--- oecn_base_fonts/reportlab_patch.py
+++ oecn_base_fonts/reportlab_patch.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    pdf report support for your language - OpenERP Module
+#    Copyright (C) 2013 Shine IT (<http://www.openerp.cn>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 # bug from :http://two.pairlist.net/pipermail/reportlab-users/2012-January/010336.html
 # get fonts name and file path
 # ff.search() may resulting error if any AFMFont resided in
